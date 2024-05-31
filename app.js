a1e66a58b434057801bfdb41db3b7051e2a50378
Andrei Krasnov <andrei.krasnov@tptlive.ee>
2024-05-31 15:02:33 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Message = require("./models/message");
+
+const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/learnexpress", {})
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use(express.json());
+
+app.get("/messages/:id", async (req, res) => {
+  try {
+    const message = await Message.findById(req.params.id);
+    if (!message) {
+      return res.status(404).json({ message: "Message not found" });
+    }
+    res.json(message);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
